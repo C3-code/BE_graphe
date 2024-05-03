82a4d55096695f0cbd5efd90be9e32f83a027316
Ce <cel_lav@yahoo.com>
2024-05-03 13:03:45 +0200

    fin_ecriture_classe_label

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -5,22 +5,68 @@ import org.insa.graphs.model.Node;
 public class Label implements Comparable<Label>{ //les elements donnes doivent etre comparables 
     private Node sommetCourant;
     private boolean marque;
-    private int coutRealise;
+    private float coutRealise;
     private Node pere;
 
+    public Label(Node noeud)  {
+        this.sommetCourant = noeud;
+        this.marque = false;
+        this.coutRealise = Float.POSITIVE_INFINITY;
+        this.pere = null;
+    }
+
     /** Getters des elements */
-    public Node getSommetCourant(Node noeud) {
-        return 0;
+    public Node getSommetCourant() {
+        return sommetCourant;
     }
 
     //retourner le cout du label 
-    public int getCost(Label label) {
+    public float getCost() {
         return coutRealise;
     }
+    
+    public boolean getMarque() {
+        return marque;
+    }
 
-    public int compareTo(Label variable) {
+    public Node getPere() {
+        return pere;
+    }
+
+    public int compareTo(Label autre) {
         // this.coutRealise et variable.coutrealise  
-        return 1;
+        int resultat;
+        if (this.getCost()<autre.getCost()) {
+            resultat = -1;
+        }
+        else if (this.getCost()==autre.getCost()){
+            resultat = 0;
+        }
+        else {
+            resultat = 1;
+        }
+        return resultat;
+    }
+
+
+
+    /** Setters des elements */
+    //modifier le sommet courant
+    public void setSommetCourant(Node sommet) {
+        this.sommetCourant = sommet;
+    }
+
+    //modifier le cout du label
+    public void setCost(float cout) {
+        this.coutRealise = cout;
+    }
+    
+    public void setMarque() {
+        this.marque = true;
+    }
+
+    public void setPere(Node father) {
+        this.pere=father;
     }
 }
 
