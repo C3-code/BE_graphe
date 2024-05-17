c270bd991bd3e5afa2378968b9509d7353acf726
Ce <cel_lav@yahoo.com>
2024-05-17 10:44:28 +0200

    capsule temporelle

---
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestPath/shortestPathTest.java
@@ -0,0 +1,27 @@
+package org.insa.graphs.algorithm.shortestPath;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+import org.insa.graphs.model.RoadInformation;
+import org.insa.graphs.model.RoadInformation.RoadType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class shortestPathTest {
+    /*Capsule temporelle: il faut implémenter les tests avec junits avec @before class et @Test (s'inspirer des tests
+    realises dans la classe PathTEST) en 
+    1) decrivant l'environnement => creation de graphes et de maps que  l'on veut tester 
+        pour cela, reprendre le fonctionnement utilise dans le fichier Launch.java (tout sauf la dernière ligne
+        qui permet de faire l'affichage. Car nous n'avons pas besoin de l'affichage)
+    2) faire les test avec la fonction assert() issue de la bib junit */
+}
